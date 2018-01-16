83a6b6e84723f79c15508e288c9a6eb55bff4234
maikx2333 <maikx@qq.com>
2018-01-16 18:18:47 +0800

    Add files via upload

---
+++ build/web-desktop/src/settings.84698.js
@@ -0,0 +1,181 @@
+_CCSettings = {
+    platform: "web-desktop",
+    groupList: [
+        "default",
+        "platform",
+        "player",
+        "gold"
+    ],
+    collisionMatrix: [
+        [
+            false,
+            false,
+            false,
+            false
+        ],
+        [
+            false,
+            false,
+            true,
+            false
+        ],
+        [
+            false,
+            true,
+            false,
+            true
+        ],
+        [
+            false,
+            false,
+            true,
+            false
+        ]
+    ],
+    rawAssets: {
+        assets: {
+            "d1R7vII7FPnJFtUSijULor": [
+                "res/texture/JoyBack.png",
+                0
+            ],
+            "33b6xlsF9BMLQlaCVM7x6F": [
+                "res/texture/JoyButton.png",
+                0
+            ],
+            "e5alGhkRRNTZqi/1CCRZ4o": [
+                "res/texture/far-bg1.png",
+                0
+            ],
+            "47+Zsy3kRLC4bRdnIE6Ngl": [
+                "res/texture/gold-bar.png",
+                0
+            ],
+            "c6x8P3hnlMarQu2/VaN54D": [
+                "res/texture/gold.png",
+                0
+            ],
+            "78fDOc/sZHQr/yJN5/9HBQ": [
+                "res/texture/near-bg.png",
+                0
+            ],
+            "f876PRW/ZA8JnvaGSlEOjA": [
+                "res/texture/panda.png",
+                0
+            ],
+            "65vxiMazNORo8tmx5ygnbF": [
+                "res/texture/platform.png",
+                0
+            ],
+            "50Jn0pUPVGnroCeIB8Xlse": [
+                "res/texture/score-board.png",
+                0
+            ],
+            "c3U4wcqPZNdZNWzShdTtdS": [
+                "res/texture/shoes-prop.png",
+                0
+            ]
+        },
+        internal: {
+            "d8HsitJHxOYqo801xBk8ev": [
+                "image/default_panel.png",
+                0
+            ]
+        }
+    },
+    assetTypes: [
+        "cc.Texture2D"
+    ],
+    launchScene: "db://assets/res/scene/game-scene.fire",
+    scenes: [
+        {
+            url: "db://assets/res/scene/game-scene.fire",
+            uuid: 0
+        }
+    ],
+    packedAssets: {
+        "01828ecbe": [
+            "0aJ7ZZKA1DLred5kifNt2V",
+            "0eHlIhrxFLHZv6Otiwg0+Y",
+            "10Q5dneCtOR6flzZ0uecnT",
+            "10T9lIqXlJzahFuRhsC5GB",
+            "13xmMgEf9KnpiB9BbAfSuy",
+            "15acrXvnxFZp5hIouYM9LG",
+            "16gf0md5pBn6uEy8Sgzhkd",
+            "1cn/eqjFBMGa0kgZtI3Gpg",
+            "1dZ7tdZChAE5wkADKoMbE8",
+            "1esIxeebRO7Kf/EzsRPhMn",
+            "1fPj8+NFFDNJNjqyMFYUFT",
+            "26zeoAQVpHB5yPhjGjIf05",
+            "27gi2aSsBHOr7CS+zn7aWR",
+            "2a10H48ixCU7SUl7HJ08ef",
+            "2cyuMcIIJHvJM3BUWD/7dO",
+            0,
+            "493VHIwDhE6quWSBYdRj8z",
+            "4emDagNlhBi4EgrKAt+FAm",
+            "56V5nyZmFKh6By/8vQ9g0+",
+            "577Z6vPwpJcIeskxGVzbzZ",
+            "5d9LyXHydIgIk4S0FHDRVC",
+            "60iDUFvzlHkKnagK7enboK",
+            "66V6D3oDpC+otqno16FQsc",
+            "6avnlV2nBKTJUvxu86c16v",
+            "6cEapZ2odMJ69IjJgdBCXx",
+            "6eKTykYsRAap4AxUWEfKjs",
+            "76O/uRJwtPtIcfYWP5Yv0R",
+            "7eA+AZBaFLaLgB2RbRtlHz",
+            "82iEaUuWlM+JWQzt7oFv7b",
+            "86ozHOrPBDYLjPqYPawyX+",
+            "88bJLU3zJDCaLaHheJbC4n",
+            "8fGSctKMhDyYNQDPiuRzKQ",
+            "976imT5ZtAg457f8PINx12",
+            "9bbPLee/tElLNA9zwReZGQ",
+            "9bvaMerUlDyary99mJa6xp",
+            "a0UNP60h9E/4NUoU6UnrMw",
+            "a3EzWCmeNKPbIYnjXqzXLW",
+            "a4a90ZpOBFGpavGsyorexT",
+            "a48fRU8MpAhaeLMm5HFPHU",
+            "a9cKvemhVHhImLuYn077xB",
+            "aciYSxpEZHUq0TUiJ3yipC",
+            "b5nIHmc4xNBK17nEt9gWz0",
+            "b6hc/TRzpL0IlcWP0XiX19",
+            "b92z4J2exHa5U8S5RLvGFL",
+            "bc7SjAZgNKHIKivtB7IoJ5",
+            "bfnLBJZ35NMJrrzjiaB1Hd",
+            "c1sRTZmuVGdbcSnStS4f7J",
+            "c4wKSMWLtEuq6A2/gVR1/q",
+            "c5ffZr/iZKj7E4iEkzK55w",
+            "c6eZRfNIVNpa++VERMgxJX",
+            "ca3lPGgJdB9Ki/MhDehewZ",
+            "cbtN/BSMxHGrDQ6qoA2eLu",
+            "cfdQaqT2BDKZGQNVktMm4v",
+            "d2nZ5crHpNCZoxJEa98OI2",
+            "d3woNl0RlLKIdzzL4pov/U",
+            "d52+aHvnFB5IfHbMmX4tTs",
+            "d8fg/bkLRMiq0Tmtg8U8xH",
+            "ddZNtwMUdNFpeqIcTKXSn6",
+            "e0MCZKlSlMcLBijDuak8vS",
+            "e56Oi53gZMWpE8x4Tas7qo",
+            "ea3xPSFlhHt4UA/fkkyxZV",
+            "f136fX9UVEe6ARNXWTaSOL",
+            "f4NVOXoAlLV6pK04iSA39q",
+            "f6W5htfY1MA5Sq+cuttjpF"
+        ]
+    },
+    orientation: "",
+    uuids: [
+        "3btiVS/odIAYYYrKhuGUYz"
+    ],
+    "md5AssetsMap": {
+        "01/01828ecbe.json": "6b808",
+        "assets/res/texture/far-bg1.png": "78b77",
+        "assets/res/texture/gold-bar.png": "a2cd4",
+        "assets/res/texture/gold.png": "f7487",
+        "assets/res/texture/JoyBack.png": "5d2a1",
+        "assets/res/texture/JoyButton.png": "c9fde",
+        "assets/res/texture/near-bg.png": "eae3a",
+        "assets/res/texture/panda.png": "157c4",
+        "assets/res/texture/platform.png": "85dd3",
+        "assets/res/texture/score-board.png": "bb802",
+        "assets/res/texture/shoes-prop.png": "01660",
+        "internal/image/default_panel.png": "cdbc9"
+    }
+}
